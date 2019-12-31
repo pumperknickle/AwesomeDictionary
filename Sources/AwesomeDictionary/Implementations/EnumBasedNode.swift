1c15823d8d39a1a8bab93722826a438fcc65acab
Pumper Knickle <pumperknickle@protonmail.com>
2019-12-31 13:25:31 -0800

    compressed codec

--- Sources/AwesomeDictionary/Implementations/EnumBasedNode.swift
+++ Sources/AwesomeDictionary/Implementations/EnumBasedNode.swift
@@ -6,18 +6,18 @@ public enum EnumBasedNode<V: Codable>: Codable {
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        let prefix = try container.decode([Bool].self, forKey: .prefix)
+        let prefix = try container.decode(String.self, forKey: .prefix)
         let value = try? container.decode(V.self, forKey: .value)
         let trueNode = try? container.decode(Self.self, forKey: .trueNode)
         let falseNode = try? container.decode(Self.self, forKey: .falseNode)
-        self = .node(prefix: prefix, value: value, trueNode: trueNode, falseNode: falseNode)
+        self = .node(prefix: prefix.bools(), value: value, trueNode: trueNode, falseNode: falseNode)
     }
     
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         switch self {
         case .node(let prefix, let value, let trueNode, let falseNode):
-            try container.encode(prefix, forKey: .prefix)
+            try container.encode(prefix.literal(), forKey: .prefix)
             if let value = value { try container.encode(value, forKey: .value) }
             if let trueNode = trueNode { try container.encode(trueNode, forKey: .trueNode) }
             if let falseNode = falseNode { try container.encode(falseNode, forKey: .falseNode) }
