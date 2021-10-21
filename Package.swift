74a0d064b45856f7784cab2a30cb0ea60dd2308b
Pumper Knickle <pumperknickle@protonmail.com>
2021-10-20 20:53:35 -0500

    benchmarks

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,11 +9,13 @@ let package = Package(
         .library(
             name: "AwesomeDictionary",
             targets: ["AwesomeDictionary"]),
+        .executable(name: "my-benchmark", targets: ["MyBenchmark"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pumperknickle/Bedrock.git", from: "0.2.3"),
+        .package(url: "https://github.com/pumperknickle/Bedrock.git", from: "0.2.4"),
         .package(url: "https://github.com/Quick/Quick.git", from: "3.1.2"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
+        .package(url: "https://github.com/apple/swift-collections-benchmark", from: "0.0.1"),
     ],
     targets: [
         .target(
@@ -22,5 +24,11 @@ let package = Package(
         .testTarget(
             name: "AwesomeDictionaryTests",
             dependencies: ["AwesomeDictionary", "Quick", "Nimble", "Bedrock"]),
+        .target(
+             name: "MyBenchmark",
+             dependencies: [
+               .product(name: "CollectionsBenchmark", package: "swift-collections-benchmark"),
+                "AwesomeDictionary"
+             ]),
     ]
 )
